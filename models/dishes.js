5477daf3480c54e2ec0bb88b67db3f6b4a945423
Uzair Ali <uzairali19b@gmail.com>
2021-11-06 19:17:48 +0500

    update project setup files

---
+++ models/dishes.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dishSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+var Dishes = mongoose.model("Dish", dishSchema);
+
+module.export = Dishes;
